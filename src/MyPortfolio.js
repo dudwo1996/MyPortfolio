6702f7fcd2229f559b23daac0e0f58ff1f373371
조영재 <wo_123@naver.com>
2021-07-28 16:54:31 +0900

    third commit

--- src/MyPortfolio.js
+++ src/MyPortfolio.js
@@ -2,19 +2,23 @@ import React from 'react'
 import './MyPortfolio.css'
 const MyPortfolio = () => {
     return (
-        <div>
-            <div className="headerWrap" style={{backgroundColor:'antiquewhite'}}>
-                <span className="nameLogo">CYJ's Portfolio</span>
-                <span className="header">My Projects</span><span className="header">My GitHub</span><span className="header">My Skills</span><span className="header">My Career</span><span className="header">About Me</span>
+        <div >
+            <div className="headerWrap">
+                <span className="nameLogo" onClick={window.scrollTo(0,0)}>CYJ's Portfolio</span>
+                <a href="#AboutMeBox"><span className="header">About Me</span></a>
+                <a href="#MyCareerBox"><span className="header">My Career</span></a>
+                <a href="#MySkillsBox"><span className="header">My Skills</span></a>
+                <a href="#MyGithubBox"><span className="header">My GitHub</span></a>
+                <a href="#MyProjectsBox"><span className="header">My Projects</span></a>
             </div>
             <div className="introSelf-Wrap">
                 <div className="introSelf">
                     -조영재-
-                </div><br/>
-                <div className="introSelf">
+                </div>
+                <div className="introSelf2">
                     리액트 개발자 포트폴리오
                 </div>
-                <div style={{ textAlign: 'center', fontSize: '50pt', color: '' }}>-</div>
+                <div style={{ textAlign: 'center', fontSize: '50pt'}}>-</div>
                 <div className="mydesc">안녕하세요. 저는 프론트엔드 개발자를 꿈꾸는 조영재입니다.</div><br/>
                 <div className="mydesc2">꼼꼼한 성격과 열정이 저의 장점입니다.</div>
             </div>
